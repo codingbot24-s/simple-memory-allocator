3ffe05bb835dca1968252fc786f3fa89ef880eb9
saadfromwin11l <primecoder8@gmail.com>
2025-12-06 18:50:43 +0000

    add free complete

--- main.c
+++ main.c
@@ -2,43 +2,41 @@
 #include <unistd.h>
 #include <pthread.h>
 
-
-// 16 bytes for memory alignment 
+// 16 bytes for memory alignment
 typedef char ALIGN[16];
 typedef union
 {
-    struct 
+    struct
     {
         size_t size;
         unsigned isFree;
         union header_t *next;
-    }s;
+    } s;
     ALIGN stub;
-    
-}header_t;
+
+} header_t;
 
 header_t *head, *tail;
 
-// lock 
-pthread_mutex_t  global_malloc_lock;
+// lock
+pthread_mutex_t global_malloc_lock;
 
-header_t* get_free_block (size_t size) 
+header_t *get_free_block(size_t size)
 {
-    header_t *curr = head;    
+    header_t *curr = head;
     while (curr)
     {
         if (curr->s.isFree && curr->s.size >= size)
         {
             return curr;
         }
-        curr = curr->s.next; 
+        curr = curr->s.next;
     }
 
     return NULL;
-    
 }
 
-void* malloc (size_t size) 
+void *malloc(size_t size)
 {
     size_t totalSize;
     void *block;
@@ -55,15 +53,16 @@ void* malloc (size_t size)
     {
         header->s.isFree = 0;
         pthread_mutex_unlock(&global_malloc_lock);
-        // we need to hide the header info from user so jumping one byte 
-        return (void*)(header + 1);
+        // we need to hide the header info from user so jumping one byte
+        return (void *)(header + 1);
     }
     totalSize = sizeof(header_t) + size;
     block = sbrk(totalSize);
-    if (block == (void*) -1) {
-		pthread_mutex_unlock(&global_malloc_lock);
-		return NULL;
-	}
+    if (block == (void *)-1)
+    {
+        pthread_mutex_unlock(&global_malloc_lock);
+        return NULL;
+    }
     header = block;
     header->s.size = size;
     header->s.isFree = 0;
@@ -78,13 +77,13 @@ void* malloc (size_t size)
     {
         tail->s.next = header;
     }
-    
+
     tail = header;
     pthread_mutex_unlock(&global_malloc_lock);
-	return (void*)(header + 1);
+    return (void *)(header + 1);
 }
 
-void free (void *block) 
+void free(void *block)
 {
     header_t *header, *temp;
     void *programBreak;
@@ -94,18 +93,39 @@ void free (void *block)
     }
 
     pthread_mutex_lock(&global_malloc_lock);
-    header = (header_t*)block - 1;
-    
+    header = (header_t *)block - 1;
+
     programBreak = sbrk(0);
 
-    // check if the block at the end of the heap  
-    
-    
+    // check if the block at the end of the heap
+    if ((char *)block + header->s.size == programBreak)
+    {
+        if (head == tail)
+        {
+            head = tail = NULL;
+        }
+        else
+        {
+            temp = head;
+            while (temp)
+            {
+                if (temp->s.next == tail)
+                {
+                    temp->s.next == NULL;
+                    tail = temp;
+                }
+                temp = temp->s.next;
+            }
+        }
+        sbrk(0 - sizeof(header_t) - header->s.size);
+        pthread_mutex_unlock(&global_malloc_lock);
+        return;
+    }
+    // not the end of heap so free but not released
+    header->s.isFree = 1;
+    pthread_mutex_unlock(&global_malloc_lock);
 }
 
-
-int main () 
+int main()
 {
-	
 }
-
